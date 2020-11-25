60845adedc3ec77138b2def68b605c63e032ffb4
Kaja Piechówka <kaja.piechowka@gmail.com>
2020-11-25 15:34:57 +0100

    pass state to parent element

--- akro-game/src/pages/GeneratedPage.js
+++ akro-game/src/pages/GeneratedPage.js
@@ -1,33 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { exampleData } from "../assets/data/exampleData";
 
 import PoseList from "../components/Poses/PosesList";
 
 require("dotenv").config();
 
-const api = process.env.REACT_APP_API;
+// const api = process.env.REACT_APP_API;
 // const apiKey = process.env.REACT_APP_API_KEY;
 
-const GeneratedPage = () => {
-  const [data, setData] = useState(false);
-
-  useEffect(() => {
-    console.log(api);
-
-    fetch(`${api}sequence/generate`, {
-      method: "GET",
-      difficulty: 2,
-      length: 5,
-    })
-      .then((response) => response.json())
-      .then((poses) => {
-        setData(poses);
-      });
-
-    console.log(data);
-  }, []);
-
-  return <PoseList data={exampleData} />;
+const GeneratedPage = (props) => {
+  return (
+    <>
+      {console.log(props)}
+      <PoseList data={exampleData} />
+    </>
+  );
 
   // if (!data) {
   //   return <h1>Wait...</h1>;
--- akro-game/src/pages/MainPage.js
+++ akro-game/src/pages/MainPage.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Dificulty from "../components/Forms/Dificulty";
 import Poses from "../components/Forms/Poses";
 import { Link } from "react-router-dom";
 
 const MainPage = () => {
+  const [dificulty, setDificulty] = useState("1");
+  const [poses, setPoses] = useState("10");
+
+  const updateDificulty = (value) => {
+    setDificulty(value);
+  };
+  const updatePoses = (value) => {
+    setPoses(value);
+  };
+
   return (
     <div className="container main">
       <h1 className="main__title">
@@ -12,10 +22,15 @@ const MainPage = () => {
       </h1>
       <div className="form">
         <div className="form__holder">
-          <Dificulty />
-          <Poses />
+          <Dificulty update={updateDificulty} />
+          <Poses update={updatePoses} />
         </div>
-        <Link to="/generated" className="form__submit">
+        <Link
+          to={{
+            pathName: "/generated",
+          }}
+          className="form__submit"
+        >
           Generate
         </Link>
       </div>
