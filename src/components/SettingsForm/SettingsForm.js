bf2100a986122c79cd144c0ac682ea067c77c962
Kaja <kaja.piechowka@gmail.com>
2022-09-23 07:31:29 +0200

    slider styles start

--- src/components/SettingsForm/SettingsForm.js
+++ src/components/SettingsForm/SettingsForm.js
@@ -1,10 +1,60 @@
-import { Slider } from '@mui/material';
-import { useContext } from 'react';
+import { Slider, styled } from '@mui/material';
+import { useContext, useState } from 'react';
 import SettingsContext from '../../assets/context/SettingsContext';
 
 function valuetext(value) {
   return `${value}`;
 }
+
+// const theme = createTheme({
+//   palette: {
+//     primary: { main: '#fff500' },
+//     secondary: { main: '#ff7a00' },
+//     error: { main: '#ff0000' },
+//     success: { main: '#8de47d' },
+//     neutral: { main: '#19d3fc' },
+//   },
+// });
+
+const PrettoSlider = styled(Slider)(({ props }) => ({
+  color: props ? '#fff500' : '#ff7a00',
+  height: 8,
+  '& .MuiSlider-track': {
+    border: 'none',
+  },
+  '& .MuiSlider-thumb': {
+    height: 24,
+    width: 24,
+    backgroundColor: '#fff',
+    border: '2px solid currentColor',
+    '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+      boxShadow: 'inherit',
+    },
+    '&:before': {
+      display: 'none',
+    },
+  },
+  '& .MuiSlider-valueLabel': {
+    lineHeight: 1.2,
+    fontSize: 12,
+    background: 'unset',
+    padding: 0,
+    width: 32,
+    height: 32,
+    borderRadius: '50% 50% 50% 0',
+    backgroundColor: '#fff500',
+    color: '#000000',
+    transformOrigin: 'bottom left',
+    transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+    '&:before': { display: 'none' },
+    '&.MuiSlider-valueLabelOpen': {
+      transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+    },
+    '& > *': {
+      transform: 'rotate(45deg)',
+    },
+  },
+}));
 
 const SettingsForm = () => {
   const { setContext } = useContext(SettingsContext);
@@ -27,8 +77,7 @@ const SettingsForm = () => {
   return (
     <>
       <p className="content__text">Number of Poses:</p>
-      <Slider
-        color="primary"
+      <PrettoSlider
         aria-label="poses"
         getAriaValueText={valuetext}
         defaultValue={5}
@@ -37,12 +86,12 @@ const SettingsForm = () => {
         max={30}
         min={5}
         step={5}
+        props={false}
         valueLabelDisplay="auto"
       />
       <p className="content__text">Choose Difficulty:</p>
-      <Slider
+      <PrettoSlider
         aria-label="difficulty"
-        color="secondary"
         getAriaValueText={valuetext}
         defaultValue={1}
         onChange={valueSetDifficulty}
@@ -50,6 +99,7 @@ const SettingsForm = () => {
         min={1}
         max={3}
         step={1}
+        props
         valueLabelDisplay="auto"
       />
     </>
